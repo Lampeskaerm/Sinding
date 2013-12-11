f6a19495c0086e43a3104dd54e634ef477aeac97
Anne-Katrine Thisted Binder <aktb@fambi.dk>
2013-12-11 10:33:53 +0100

    Publish

---
+++ Assets/Follower.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follower : MonoBehaviour {
+	
+	float minDist = 50;
+	float maxDist = 100;
+	float distZ, distX;
+	Vector3 fPos;
+	
+	
+	// Use this for initialization
+	void Start () {
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		followDist ();
+	}
+	
+	void followDist () {
+		
+	}
+}
---
+++ Assets/Movements.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movements : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool up = Input.GetKey (KeyCode.UpArrow);
+
+		if (up) {
+			transform.Translate (new Vector3 (0, 0, 3) * Time.deltaTime);
+		}
+	}
+}
